6e19f18356998fff7e760902ce3ffd1ca59c0905
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-27 13:55:18 +0900

    blogsテーブル作成

--- db/migrate/20230727043948_create_blogs.rb
+++ db/migrate/20230727043948_create_blogs.rb
@@ -1,8 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration[6.1]
   def change
     create_table :blogs do |t|
-      t.string :title
-      t.string :content
+      t.string :title, null: false, index: true
+      t.string :content, null: false
 
       t.timestamps
     end
